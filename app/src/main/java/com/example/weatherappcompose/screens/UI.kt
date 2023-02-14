55f4119000621f2a2dfe58d68b0d14430c38add4
Sarvar <arifdjanovsarvar@gmail.com>
2023-02-14 15:24:40 +0500

    Phase 4
    *Added model
    *Added getData() parsing function
    *Updated TabLayout() function

--- app/src/main/java/com/example/weatherappcompose/screens/UI.kt
+++ app/src/main/java/com/example/weatherappcompose/screens/UI.kt
@@ -13,11 +13,12 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import com.example.weatherappcompose.data.WeatherModel
 import com.example.weatherappcompose.ui.theme.BlueWhite
 
-@Preview (showBackground = true)
+
 @Composable
-fun ListItem(){
+fun ListItem(item: WeatherModel){
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -34,12 +35,16 @@ fun ListItem(){
             Column(
                 modifier = Modifier.padding(start = 8.dp,top = 4.dp, bottom = 4.dp),
             ) {
-                Text(text = "12:00")
-                Text(text = "Sunny", color = Color.White)
+                Text(text = item.time)
+                Text(text = item.condition, color = Color.White)
             }
-            Text(text = "23°C", color = Color.White, style = TextStyle(fontSize = 25.sp))
+            Text(
+                text = item.currentTemp.ifEmpty {"${item.maxTemp}/${item.minTemp}°C"},
+                color = Color.White,
+                style = TextStyle(fontSize = 25.sp)
+            )
             AsyncImage(
-                model = "https://cdn.weatherapi.com/weather/64x64/day/122.png",
+                model = "https:${item.icon}",
                 contentDescription = "img5",
                 modifier = Modifier
                     .size(35.dp)
